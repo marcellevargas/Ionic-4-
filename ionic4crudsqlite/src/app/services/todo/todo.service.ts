eaee4cede7d0fa7ca20462cb3e222a8bf6d27a19
Marcelle Vargas <marcelle@stbr.com.br>
2019-08-31 13:54:11 -0300

    Added provider service'Todo'

--- ionic4crudsqlite/src/app/services/todo/todo.service.ts
+++ ionic4crudsqlite/src/app/services/todo/todo.service.ts
@@ -13,21 +13,86 @@ export class Todo {
 }
 
 export class TodoService {
-  constructor(private sqlite: SQLite, private dataBase: DatabaseService) { }
+  constructor(private sqlite: SQLite, private databaseService: DatabaseService) { }
 
   private insert(todo: Todo) {
-    const sql  = ['INSERT INTO todo (name,description,status) values(?,?,?)'];
-    const data = [todo.name, todo.description, todo.status];
+    return this.databaseService.getDB()
+    .then((db: SQLiteObject) => {
+      const sql  = 'INSERT INTO todo (name,description,status) values(?,?,?)';
+      const data = [todo.name, todo.description, todo.status];
+
+      return db.executeSql(sql, data)
+      .then(() => {
+        console.log('INSERT INTO todo' + todo.name + ' ' + todo.description + ' ' + todo.status);
+        return true;
+      })
+      .catch((e) => {
+        console.error(e.message);
+      });
+    });
   }
 
   private update(todo: Todo) {
-    const sql  = ['UPDATE todo SET name = ? ,description = ?, status = ? WHERE id = ?'];
-    const data = [todo.name, todo.description, todo.status, todo.id];
+    return this.databaseService.getDB()
+    .then((db: SQLiteObject) => {
+      const sql  = 'UPDATE todo SET name = ? ,description = ?, status = ? WHERE id = ?';
+      const data = [todo.name, todo.description, todo.status, todo.id];
+
+      return db.executeSql(sql, data)
+      .then(() => {
+        console.log('UPDATE todo' + todo.name + ' ' + todo.description + ' ' + todo.status);
+        return true;
+      })
+      .catch((e) => {
+        console.error(e.message);
+      });
+    });
   }
 
   private delete(todo: Todo) {
-    const sql  = ['DELETE FROM todo WHERE id = ?'];
-    const data = [todo.id];
+    return this.databaseService.getDB()
+    .then((db: SQLiteObject) => {
+      const sql  = 'DELETE FROM todo WHERE id = ?';
+      const data = [todo.id];
+
+      return db.executeSql(sql, data)
+      .then(() => {
+        console.log('UPDATE todo' + todo.name + ' ' + todo.description + ' ' + todo.status);
+        return true;
+      })
+      .catch((e) => {
+        console.error(e.message);
+      });
+    });
+  }
+
+  public getTodo() {
+    return this.databaseService.getDB()
+    .then((db: SQLiteObject) => {
+      const sql = 'SELECT * FROM todo';
+      return db.executeSql(sql, [])
+      .then((data: any) => {
+        if (data.rows.length > 0) {
+          const todos: any[] = [];
+          for (let i = 0; i < data.rows.length; i++) {
+            const item = data.rows.item(i);
+            const todo = new Todo();
+
+            todo.id          = item.id;
+            todo.name        = item.name;
+            todo.description = item.description;
+            todo.status      = item.status;
+
+            todos.push(todo);
+          }
+          return todos;
+        } else {
+          return [];
+        }
+      })
+      .catch((e) => console.error('Erro TodoService getTodo: ', e.message));
+    })
+    .catch((e) => console.error('Erro TodoService getTodo:', e.message));
   }
 }
 
